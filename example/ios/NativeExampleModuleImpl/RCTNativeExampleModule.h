6465a2c37b320bd518a3b0b10ef8e7b932316d38
denissb <spawnenius@gmail.com>
2022-07-26 15:30:00 +0200

    feat: added iOS implementation

---
+++ example/ios/NativeExampleModuleImpl/RCTNativeExampleModule.h
@@ -0,0 +1,16 @@
+//
+//  RCTNativeSampleModule.h
+//  TurboModuleSampleProject
+//
+//  Created by Deniss Borisovs on 2022-06-18.
+//
+
+#ifndef RCTNativeSampleModule_h
+#define RCTNativeSampleModule_h
+
+#include "NativeExampleModule.h"
+
+@interface NativeExampleModule : NSObject<NativeExampleModuleSpec>
+@end
+
+#endif /* RCTNativeExampleModule_h */
